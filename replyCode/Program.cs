3e6e778904dc1c92fe1fad676dc287d80f8b2b04
leonardo-pieraccioli <leonardo.pieraccioli@gmail.com>
2021-03-11 17:00:48 +0100

    costanti 2

--- replyCode/Program.cs
+++ replyCode/Program.cs
@@ -7,19 +7,20 @@ using System.Threading.Tasks;
 
 namespace replyCode
 { 
-    public const int MIN_DIM 10;
-    public const int MAX_DIM 6000;
-    public const int MAX_RICOMPENSA 200000000;
-    //EDIFICI
-    public const int MAX_EDIFICI 350000;
-    public const int MAX_PESO_LATENZA 100;
-    public const int MAX_PESO_CONNESSIONE 100;
-    //ANTENNE     
-    public const int MAX_ANTENNE 60000;
-    public const int MAX_DISTANZA 6000;
-    public const int MAX_VEL_CONNESSIONE 10000;
+
         
     class Program {
+        public const int MIN_DIM = 10;
+        public const int MAX_DIM = 6000;
+        public const int MAX_RICOMPENSA = 200000000;
+        //EDIFICI
+        public const int MAX_EDIFICI = 350000;
+        public const int MAX_PESO_LATENZA = 100;
+        public const int MAX_PESO_CONNESSIONE = 100;
+        //ANTENNE     
+        public const int MAX_ANTENNE = 60000;
+        public const int MAX_DISTANZA = 6000;
+        public const int MAX_VEL_CONNESSIONE = 10000;
         static void Main(string[] args)
         {
         }
