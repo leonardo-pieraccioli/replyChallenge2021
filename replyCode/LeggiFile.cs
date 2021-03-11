8d4e1676688d6555494ef16245f69476f3e489f4
leonardo-pieraccioli <leonardo.pieraccioli@gmail.com>
2021-03-11 18:36:54 +0100

    correzione leggifile e Program

--- replyCode/LeggiFile.cs
+++ replyCode/LeggiFile.cs
@@ -9,7 +9,7 @@ namespace replyCode
     class LeggiFile
     {
         private List<Edificio> edifici = new List<Edificio>();
-        private List<Antenna> antenne = new List<Edificio>();
+        private List<Antenna> antenne = new List<Antenna>();
         private int numB; 
         private int numA;
         private int reward; 
@@ -17,11 +17,11 @@ namespace replyCode
         private int y; 
 
 
-        public static void leggiFile(){
+        public void leggiFile(){
             int i;
-            System.IO.StreamReader file= new System.IO.StreamReader(@"C:\Users\francesca\Source\Repos\leonardo-pieraccioli\replyCode\replyCode\source.txt")
-            String riga; 
-            String[] valori; 
+            System.IO.StreamReader file = new System.IO.StreamReader(@"C:\Users\francesca\Source\Repos\leonardo-pieraccioli\replyCode\replyCode\source.txt");
+            string riga; 
+            string[] valori; 
             riga= file.ReadLine(); 
             valori=riga.Split(' ');
             x=Int32.Parse(valori[0]);
@@ -31,17 +31,16 @@ namespace replyCode
             numB=Int32.Parse(valori[0]);
             numA= Int32.Parse(valori[1]);
             reward=Int32.Parse(valori[2]);
-            rew=reward;
             
             for(i=0;i<numB; i++){
                 riga=file.ReadLine();
                 valori=riga.Split(' ');
-                edifici.Add(new Edificio(Int32.Parse(valori[0]),Int32.Parse(valori[1]), Int32.Parse(valori[2]), Int32.Parse(valori[3]));
+                edifici.Add(new Edificio(Int32.Parse(valori[0]),Int32.Parse(valori[1]), Int32.Parse(valori[2]), Int32.Parse(valori[3])));
             }
             for(i=0;i<numA; i++){
                 riga=file.ReadLine();
                 valori=riga.Split(' ');
-                antenne.Add(new Antenna(i, Int32.Parse(valori[0]),Int32.Parse(valori[1]));
+                antenne.Add(new Antenna(i, Int32.Parse(valori[0]),Int32.Parse(valori[1])));
             }
             file.Close();
             edifici.Sort();
@@ -54,17 +53,17 @@ namespace replyCode
 
         }
         
-        public static void stampaEdifici(){
+        public void stampaEdifici(){
             System.Console.WriteLine("ci sono {0} edifici\n", numB);
             foreach(Edificio e in edifici){
-                System.Console.WriteLine(e.getX+' '+e.getY+' '+e.getPesoLatenza+' '+e.getPesoConnessione);
+                System.Console.WriteLine(e.getX()+' '+e.getY()+' '+e.getPesoLatenza()+' '+e.getPesoConnessione());
             }
         }
 
-        public static void stampaAntenne(){
+        public void stampaAntenne(){
             System.Console.WriteLine("ci sono {0} antenne\n", numA);
             foreach(Antenna a in antenne){
-            System.Console.WriteLine(a.getID+' '+a.getDistMax+' '+a.getV);
+            System.Console.WriteLine(a.getID()+' '+a.getDistMax()+' '+a.getV());
             }
         }
     }
--- replyCode/Program.cs
+++ replyCode/Program.cs
@@ -13,6 +13,7 @@ namespace replyCode
         public const int MIN_DIM = 10;
         public const int MAX_DIM = 6000;
         public const int MAX_RICOMPENSA = 200000000;
+        public const int MAX_TENTATIVI = 100;
         //EDIFICI
         public const int MAX_EDIFICI = 350000;
         public const int MAX_PESO_LATENZA = 100;
@@ -21,8 +22,68 @@ namespace replyCode
         public const int MAX_ANTENNE = 60000;
         public const int MAX_DISTANZA = 6000;
         public const int MAX_VEL_CONNESSIONE = 10000;
+
+        public static List<Edificio> edifici = new List<Edificio>();
+        public static List<Antenna> antenne = new List<Antenna>();
+        public static int reward;
+        public static int righe, colonne;
+
         static void Main(string[] args)
         {
+            LeggiFile lettoreFile = new LeggiFile();
+            lettoreFile.leggiFile();
+
+            if (edifici.Count() < antenne.Count())
+                antenne.RemoveRange(edifici.Count(), antenne.Count() - edifici.Count());
+            
+            posizionaAntenne();
+            LeggiFile.componiOutput(Edifici, antenne);
+        }
+
+        public void posizionaAntenne()
+        {
+            int nTentativi = 0;
+
+            //posiziona antenne su edifici ordinati in base alla velocità 
+            for(int i=0; i<Math.Min(edifici.Count(), edifici.Count()); i++)
+            {
+                edifici[i].setAntenna(antenne[i]);    
+            }
+
+            //terminazione quando tutti gli edifici sono coperti e nTentativi è maggiore di MAX_TENTATIVI
+            while (nTentativi <= MAX_TENTATIVI && !edificiAssegnati())
+            {
+                //verifica di possibile avvicinamento di alcune antenne in cui si usa nTentativi 
+                foreach(Antenna a in antenne)
+                {
+                    //trovare edificio più vicino escluso quello assegnato
+
+                    //avvicinamento
+
+                }
+                //calcola soluzione, aggiorna migliore 
+            }
+
+        }
+
+        public int calcolaScore(Edificio e, Antenna a)
+        {
+            //attenzione ai nomi dei getters 
+            return e.getPesoConnessione() * a.getV() - e.getPesoLatenza() * calcolaDistanza(e, a);
+        }
+        public static int calcolaDistanza(Edificio e, Antenna a)
+        {
+            return (Math.Abs(e.getX() - a.getX() + Math.Abs(e.getY() - a.getY()));
+        }
+
+        public bool edificiAssegnati()
+        {
+            foreach (Edificio e in edifici)
+            {
+                if (e.getAntenna() == null)
+                    return false;
+            }
+            return true;
         }
 
     }
