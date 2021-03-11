d6779b235627e10f455f267f600cd783332f2bd2
MartinaPlumari <martinaplumari@hotmail.it>
2021-03-11 17:49:43 +0100

    Classi antenna e edificio

--- replyCode/Antenna.cs
+++ replyCode/Antenna.cs
@@ -6,7 +6,49 @@ using System.Threading.Tasks;
 
 namespace replyCode
 {
-    class Antenna
+    class Antenna : IComparable<Antenna>
     {
+        private int id;
+        private int x;
+        private int y;
+        private int distMax;
+        private int v;
+
+        public Antenna(int id, int distMax, int v)
+        {
+            this.id = id;
+            this.distMax = distMax;
+            this.v = v;
+        }
+
+        public int CompareTo(Antenna other)
+        {
+            return this.v.CompareTo(other.v);
+        }
+
+        public int getX()
+        {
+            return this.x;
+        }
+
+        public int getY()
+        {
+            return this.y;
+        }
+
+        public int getID()
+        {
+            return this.id;
+        }
+
+        public int getDistMax()
+        {
+            return this.distMax;
+        }
+
+        public int getV()
+        {
+            return this.v;
+        }
     }
 }
--- replyCode/Edificio.cs
+++ replyCode/Edificio.cs
@@ -6,7 +6,56 @@ using System.Threading.Tasks;
 
 namespace replyCode
 {
-    class Edificio
+    class Edificio : IComparable<Edificio>
     {
+        private int x;
+        private int y;
+        private int pesoLatenza;
+        private int pesoConnessione;
+        private Antenna a;
+
+        public Edificio(int x, int y, int pesoLatenza, int pesoConnessione)
+        {
+            this.x = x;
+            this.y = y;
+            this.pesoLatenza = pesoLatenza;
+            this.pesoConnessione = pesoConnessione;
+            this.a = null;
+        }
+
+        public int CompareTo(Edificio other)
+        {
+            return this.pesoConnessione.CompareTo(other.pesoConnessione);
+        }
+
+        public int getX()
+        {
+            return this.x;
+        }
+
+        public int getY()
+        {
+            return this.y;
+        }
+
+        public int getPesoLatenza()
+        {
+            return this.pesoLatenza;
+        }
+
+        public int getPesoConnessione()
+        {
+            return this.pesoConnessione;
+        }
+
+        public Antenna getAntenna()
+        {
+            return this.a;
+        }
+
+        public void setAntenna(Antenna a)
+        {
+            this.a = a;
+        }
     }
 }
