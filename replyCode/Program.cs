8883b9fca7ce05e17e4b81117e1530462611684c
leonardo-pieraccioli <leonardo.pieraccioli@gmail.com>
2021-03-11 16:58:01 +0100

    costanti

--- replyCode/Program.cs
+++ replyCode/Program.cs
@@ -4,8 +4,21 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace replyCode
 { 
+    public const int MIN_DIM 10;
+    public const int MAX_DIM 6000;
+    public const int MAX_RICOMPENSA 200000000;
+    //EDIFICI
+    public const int MAX_EDIFICI 350000;
+    public const int MAX_PESO_LATENZA 100;
+    public const int MAX_PESO_CONNESSIONE 100;
+    //ANTENNE     
+    public const int MAX_ANTENNE 60000;
+    public const int MAX_DISTANZA 6000;
+    public const int MAX_VEL_CONNESSIONE 10000;
+        
     class Program {
         static void Main(string[] args)
         {
